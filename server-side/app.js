80deeae4170fd4ba20c9a94d3a59d44f07890981
Tuan Nguyen <atuannguyen1101@gmail.com>
2018-10-19 22:50:49 -0400

    First route

--- server-side/app.js
+++ server-side/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors');
+const http = require('http');
+const https = require('https');
+const apiKey = "k8X97RqaamacWzZnyGrG7SJAAyhZwh9j"
 require('dotenv').config();
 
 app.use(bodyParser.json());
@@ -12,5 +15,16 @@ app.listen(process.env.PORT || 8000, () => {
 });
 
 app.get('/api', (req, res) => {
-    
-})
+    var run = async function() {
+        var city = "New York";
+        var a = await new Promise(resolve => {
+            https.get(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=${apiKey}&city=${city}`, (data) => {
+                data.setEncoding('utf8');
+                data.on('data', function (body) {
+                    resolve(body);
+                });
+            });
+        })
+        res.send(a);
+    }()
+});
